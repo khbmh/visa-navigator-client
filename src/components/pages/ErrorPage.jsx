6d8b6b87c4a669595f2569a9791070eeba762d40
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2024-12-07 21:38:49 +0600

    error page update

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 function ErrorPage() {
   return (
-    <div className="flex flex-col gap-16 items-center justify-around py-20 h-screen text-center">
+    <div className="flex flex-col gap-16 items-center justify-around py-28 h-screen text-center">
       <div className="flex flex-col gap-2 items-center justify-center">
         <img
           className="w-[80vw] max-w-[400px]"
-          src="https://cdn3d.iconscout.com/3d/premium/thumb/404-error-11675546-9496341.png"
+          src="https://static-00.iconduck.com/assets.00/404-page-not-found-illustration-2048x998-yjzeuy4v.png"
           alt="Page not found"
         />
-        <h1 className="text-red-400 font-bold text-4xl">Page Not Found</h1>
+        <h1 className="text-pink-400 font-bold text-4xl">Page Not Found</h1>
       </div>
-      <button className="btn btn-outline btn-success">
+      <button className="btn mid">
         <Link to="/">Back to Home</Link>
       </button>
     </div>
